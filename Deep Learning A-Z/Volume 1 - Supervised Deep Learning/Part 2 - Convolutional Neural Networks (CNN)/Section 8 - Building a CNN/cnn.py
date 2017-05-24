af153f12f8ff85a2847b460724b430db3b6dccec
seichiyami <huebert.brandon@gmail.com>
2017-05-24 14:46:49 -0700

    Ignore Saved Models and Improve CNN
    Modify .gitignore to ignore files beginning with model. Add more layers
    and make script easier to modify.

--- Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
+++ Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
@@ -11,18 +11,25 @@ Created on Wed May 10 14:19:30 2017
 
 # Importing the Keras libraries and packages
 from keras.models import Sequential
-from keras.layers import Convolution2D
+from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
+IMAGE_INPUT_X = 64
+IMAGE_INPUT_Y = 64
+BATCH_SIZE = 32
+EPOCHS = 25
 # Initialising the CNN
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Convolution2D(32, kernel_size = (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(32, kernel_size = (3, 3), input_shape = (IMAGE_INPUT_X, IMAGE_INPUT_Y, 3), activation = 'relu'))
 
 # Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Conv2D(32, kernel_size = (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Step 3 - Flattening
@@ -30,6 +37,7 @@ classifier.add(Flatten())
 
 # Step 4 - Full Connection
 classifier.add(Dense(units = 128, activation = 'relu'))
+# classifier.add(Dense(units = 128, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compilling the CNN
@@ -47,20 +55,22 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
         'dataset/training_set',
-        target_size=(64, 64),
-        batch_size=32,
+        target_size=(IMAGE_INPUT_X, IMAGE_INPUT_Y),
+        batch_size=BATCH_SIZE,
         class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(
         'dataset/test_set',
-        target_size=(64, 64),
-        batch_size=32,
+        target_size=(IMAGE_INPUT_X, IMAGE_INPUT_Y),
+        batch_size=BATCH_SIZE,
         class_mode='binary')
 
 classifier.fit_generator(
         training_set,
-        steps_per_epoch=8000,
-        epochs=25,
+        steps_per_epoch=8000/BATCH_SIZE,
+        epochs=EPOCHS,
         validation_data=test_set,
-        validation_steps=2000,
-        workers = 8)
+        validation_steps=2000/BATCH_SIZE)
+
+classifier.save("model_2.h5")
+del classifier
