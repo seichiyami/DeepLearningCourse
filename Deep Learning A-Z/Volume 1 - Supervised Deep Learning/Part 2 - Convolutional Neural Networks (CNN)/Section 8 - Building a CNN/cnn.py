813ef29f865cd7a24d6b37372dded1451a88c795
seichiyami <huebert.brandon@gmail.com>
2017-05-24 23:26:44 -0700

    Add single prediction

--- Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
+++ Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn.py
@@ -16,8 +16,8 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
-IMAGE_INPUT_X = 64
-IMAGE_INPUT_Y = 64
+IMAGE_INPUT_X = 128
+IMAGE_INPUT_Y = 128
 BATCH_SIZE = 32
 EPOCHS = 25
 # Initialising the CNN
@@ -37,11 +37,11 @@ classifier.add(Flatten())
 
 # Step 4 - Full Connection
 classifier.add(Dense(units = 128, activation = 'relu'))
-# classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 128, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compilling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'adadelta', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Part 2 - Fitting the CNN to the images
 from keras.preprocessing.image import ImageDataGenerator
@@ -72,5 +72,24 @@ classifier.fit_generator(
         validation_data=test_set,
         validation_steps=2000/BATCH_SIZE)
 
-classifier.save("model_2.h5")
+# use model to predict single image
+import numpy as np
+from keras.preprocessing import image
+
+single_image = image.load_img("dataset/single_prediction/cat_or_dog_2.jpg", target_size = (IMAGE_INPUT_X, IMAGE_INPUT_Y))
+single_image = image.img_to_array(single_image)
+single_image = np.expand_dims(single_image, axis = 0)
+result = classifier.predict(single_image)
+
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = "dog"
+else:
+    prediction = "cat"
+
+
+
+
+
+classifier.save("model_3.h5")
 del classifier
